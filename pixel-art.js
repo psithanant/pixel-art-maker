f124a3aaa2fc8b9eb476e906c47d1c6aed037498
Aom <p.sithanant@gmail.com>
2017-01-23 15:14:54 -0800

    bonus added

--- pixel-art.js
+++ pixel-art.js
@@ -3,18 +3,26 @@ document.addEventListener("DOMContentLoaded", function() {
   var outer = document.getElementById('outer');
   for (var i = 0; i < 5029; i++) {
     var div = document.createElement('div');
-    div.className = 'divSquare'
+    div.className = 'divSquare';
     outer.appendChild(div);
   }
 });
-document.getElementById('outer').addEventListener('click', function(event) {
-    event.target.style["background-color"] = brushColor;
+document.getElementById('outer').addEventListener('mouseover', function(event) {
+   if (event.buttons == 1) {
+       event.target.style["background-color"] = brushColor;
+   }
 });
 
 var brushColor;
 
 document.getElementById('palette').addEventListener('click', function(event) {
   brushColor = event.target.style["background-color"];
-  var indicator = document.getElementById('indicator-color')
+  var indicator = document.getElementById('indicator-color');
   indicator.style["background-color"] = brushColor;
-})
+});
+
+document.getElementById('outer').addEventListener('mouseover', function(event) {
+    if (event.buttons === 1) {
+        event.target.style["background-color"] = brushColor;
+   }
+});
